995e07f4ddc3fe1cc87be92066c7f3601892d9a4
rohit.tolety <32849841+phantom7knight@users.noreply.github.com>
2018-11-08 20:01:53 -0800

    Added Engine Class which will be used for initializations

---
+++ OpenGLEngine/Engine.cpp
@@ -0,0 +1,28 @@
+#include "Engine.h"
+
+
+
+Engine::Engine()
+{
+}
+
+
+Engine::~Engine()
+{
+}
+
+void Engine::Initialize()
+{
+}
+
+void Engine::Update()
+{
+}
+
+void Engine::Render()
+{
+}
+
+void Engine::Destroy()
+{
+}
---
+++ OpenGLEngine/Engine.h
@@ -0,0 +1,15 @@
+#pragma once
+class Engine
+{
+public:
+	Engine();
+	~Engine();
+
+	void Initialize();
+	void Update();
+	void Render();
+	void Destroy();
+
+
+};
+
