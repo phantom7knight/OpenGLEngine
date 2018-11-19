a550e5ce7f1c1a395bb3c929cd9d360ab70c3d07
rohit.tolety <32849841+phantom7knight@users.noreply.github.com>
2018-11-19 13:51:52 -0800

    Removed all the lines which are causing errors in the FBO

--- OpenGLEngine/src/FrameBuffer.cpp
+++ OpenGLEngine/src/FrameBuffer.cpp
@@ -1,6 +1,12 @@
 #include "FrameBuffer.h"
-#include "../Game.h"
+//#include "../Game.h"
 #include "glad.h"
+
+
+#define Screen_Width1  1366
+#define Screen_Height1  768 
+
+
 
 FrameBuffer::FrameBuffer()
 {
@@ -15,18 +21,18 @@ FrameBuffer::~FrameBuffer()
 unsigned int FrameBuffer::CreateTextureFBO(unsigned int texture_name,int format)
 {
 	//Generate Texture
-	glGenTextures(1, &texture_name);
-	
-	//Bind Texture
-	glBindTexture(GL_TEXTURE_2D, texture_name);
-	
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Screen_Width, Screen_Height, 0, GL_RGB, GL_UNSIGNED_INT, NULL);
+	//glGenTextures(1, &texture_name);
+	//
+	////Bind Texture
+	//glBindTexture(GL_TEXTURE_2D, texture_name);
+	//
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Screen_Width1, Screen_Height1, 0, GL_RGB, GL_UNSIGNED_INT, NULL);
 
 	//Texture Properties
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 	return texture_name;
 		
@@ -41,10 +47,10 @@ void FrameBuffer::SetFrameBuffer(unsigned int & FBO, unsigned int& texture)
 
 void FrameBuffer::FrameBufferBind(unsigned int & FBO)
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+	//glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 }
 
 void FrameBuffer::FrameBufferUnBind(unsigned int & FBO)
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
